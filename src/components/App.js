a2d7eaa22038db9832275862c8dce0c59258b8e3
geewey <geewey@gmail.com>
2020-05-01 01:27:26 +0800

    implemented logic for assigning and displaying 13 unique cards into state

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,59 @@
 import React, { useState } from "react";
+import allCards from "../helpers/cardData";
 import "./App.css";
 
 const App = () => {
   const [hand, setHand] = useState([]);
+  const [playedCards, setPlayedCards] = useState([]);
 
-  const allCardsArray = [];
+  // logic for dealing a hand with 13 random cards
+  const dealHand = (allCards) => {
+    let hand = [];
+    let availableCards = [...allCards];
 
-  const code = "1h";
-  const cardName = "whatever";
-  // create a button to "deal" cards
-  // show all cards
-  // create a button to "undeal cards"
+    while (hand.length < 13) {
+      let randomCard =
+        availableCards[Math.floor(Math.random() * availableCards.length)];
+
+      hand.push(randomCard);
+      availableCards = availableCards.filter((card) => randomCard !== card);
+    }
+    setHand(hand);
+  };
+
+  // logic for selecting a card
+  const handleSelectCard = (card) => {
+    console.log(card);
+  };
+
+  // HeaderContainer for "Deal button" logic
+  const handleDeal = (allCards) => {
+    dealHand(allCards);
+  };
 
   return (
     <div>
-      <p>Your hand</p>
-      <img className="small-card" src={`/cards/${code}.png`} alt={cardName} />
+      <div>
+        {/* clears played cards, assigns 13 new random cards */}
+        <button onClick={() => handleDeal(allCards)}>Deal button</button>
+      </div>
+      <div>
+        <p>Your hand (13 random cards)</p>
+        {hand.map((card) => {
+          return (
+            <img
+              className="small-card"
+              src={`/cards/${card.code}.png`}
+              alt={card.name}
+              key={card.name}
+              onClick={() => handleSelectCard(card)}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <p>Played cards</p>
+      </div>
     </div>
   );
 };
