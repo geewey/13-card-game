d205f2893dc388ed5ef10e6995230db90746c4d7
geewey <geewey@gmail.com>
2020-07-30 21:51:53 +0800

    update game log for rule url to be clickable

--- src/containers/GameLogContainer.js
+++ src/containers/GameLogContainer.js
@@ -5,7 +5,24 @@ const GameLogContainer = ({ gameLog }) => {
     <div className="game-log-container">
       <h2>Game Log</h2>
       {gameLog.map((log) => {
-        return <p key={100000000 * Math.random()}>{`• ${log}`}</p>;
+        if (log.includes("http")) {
+          let index = log.indexOf("https");
+          let url = log.slice(index);
+          let message = log.slice(0, index);
+          return (
+            <p key={100000000 * Math.random()}>
+              {`• ${message}`}{" "}
+              <a
+                href={`${url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="url"
+              >{`${url}`}</a>
+            </p>
+          );
+        } else {
+          return <p key={100000000 * Math.random()}>{`• ${log}`}</p>;
+        }
       })}
     </div>
   );
